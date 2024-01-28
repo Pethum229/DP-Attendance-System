7bb3ba9c16ac4cb06b1e36b1f9f6df54a0bb55d9
Pethum229 <pathumpp99@gmail.com>
2024-01-28 16:15:28 +0530

    Database Query Updates

--- time_table.php
+++ time_table.php
@@ -22,32 +22,21 @@
                         <?php
                             include "db_connection.php";
 
-                            $sql = $db->prepare("SELECT dt.`StudentID`,s.`StudentName` FROM `daily_time_tables` dt 
+                            $time_table = $db->prepare("SELECT dt.`StudentID`,s.`StudentName` FROM `daily_time_tables` dt 
                                                         JOIN `students` s ON dt.`StudentID` = s.`StudentID`
                                                             WHERE dt.DateNTime = ?");
-                            $sql->execute(array('1'));
-                            while ($row = $sql -> fetch (PDO::FETCH_ASSOC)){      
+                            $time_table->execute(array('18'));
+                            while ($row = $time_table -> fetch (PDO::FETCH_ASSOC)){      
                         ?>
                             <tr>
-                                <td><?php echo $row['ID'] ?></td>
                                 <td><?php echo $row['StudentID'] ?></td>
                                 <td><?php echo $row['StudentName'] ?></td>
-                                <td><?php echo $row['PhoneNumber'] ?></td>
-                                <td><?php echo $row['ProjectsCompleted'] ?></td>
-                                <td>
-                                    <a href=""><img class="buttonIcons" src="../images/view-eye-interface-symbol.png" alt="View Button"></a>
-                                    <a href=""><img class="buttonIcons" src="../images/edit-button.png" alt="Edit Button"></a>
-                                    <a href=""><img class="buttonIcons" src="../images/delete-button.png" alt="Delete Button"></a>
-                                </td>
                             </tr>
                         <?php
                         }
                         ?>
                     </tbody>
                 </table>
-                <button type="submit" class="btn btn-success pull-right" onclick="Export()">
-                    <i class="fa fa-file-excel-o fa-fw">Export to Excel</i>
-                </button>
                 <!-- Structre of Time Table <-End-> -->
         </div>
     </section>
