592d2314f9b91fced029f6333f1fee71d6b9bb90
Pethum229 <pathumpp99@gmail.com>
2024-01-28 15:54:42 +0530

    Database Query Updates

--- AdminPortal/timetable.php
+++ AdminPortal/timetable.php
@@ -1,6 +1,5 @@
 <?php
 
-$currentDate = date('Y-m-d');
 $currentDayName = date('l');
 // echo $currentDayName;
 
@@ -28,17 +27,66 @@ switch($currentDayName){
         break;   
 }
 
-// Set 5.30 AM - 7.30 AM Student List
-echo "<h1>$currentDayName 5.30AM - 7.30AM Student List ($currentDate)</h1>";
-
 $timeTable01 = $day.'1';
+$timeTable02 = $day.'2';
+$timeTable03 = $day.'3';
+$timeTable04 = $day.'4';
+$timeTable05 = $day.'5';
+$timeTable06 = $day.'6';
+$timeTable07 = $day.'7';
+$timeTable08 = $day.'8';
+
 
 try{
     include_once "../db_connection.php";
 
-    $timeTable = $db->prepare("INSERT INTO `daily_time_tables` (`StudentID`,`DateNTime`) SELECT `StudentID`,`DateAndTime` FROM `time_tables` WHERE `DataAndTime`=? ORDER BY `Count` ASC LIMIT 25");
-    $timeTable -> execute(array($timeTable01));
+    // Delete all rows of daily_time_table
+    $deleteAllRows = $db->prepare("DELETE FROM `daily_time_tables`");
+    $deleteAllRows -> execute();
 
+    // Insert selected data from time_tables table to daily_time_table
+    $timeTable = $db->prepare("INSERT INTO `daily_time_tables` (`StudentID`,`DateNTime`)
+                                    SELECT tt.`StudentID`,
+                                           tt.`DateAndTime` FROM `time_tables` tt 
+                                            JOIN `students` s ON tt.`StudentID` = s.`StudentID`
+                                                WHERE tt.`DateAndTime`=? AND s.`IsActive`=? ORDER BY tt.`Count` ASC LIMIT 30");
+
+    // Insert 01 time slot data to daily_time_table
+    $timeTable -> execute(array($timeTable01,'1'));
+
+    // Insert 02 time slot data to daily_time_table
+    $timeTable -> execute(array($timeTable02,'1'));
+
+    // Insert 03 time slot data to daily_time_table
+    $timeTable -> execute(array($timeTable03,'1'));
+
+    // Insert 04 time slot data to daily_time_table
+    $timeTable -> execute(array($timeTable04,'1'));
+
+    // Insert 05 time slot data to daily_time_table
+    $timeTable -> execute(array($timeTable05,'1'));
+
+    // Insert 06 time slot data to daily_time_table
+    $timeTable -> execute(array($timeTable06,'1'));
+
+    // Insert 07 time slot data to daily_time_table
+    $timeTable -> execute(array($timeTable07,'1'));
+
+    // Insert 08 time slot data to daily_time_table
+    $timeTable -> execute(array($timeTable08,'1'));
+
+    echo "Daily time table was created successfully";
+
+    //Update count of time_tables
+    $countUpdate = $db->prepare("UPDATE `time_tables` tt JOIN `daily_time_tables` dt
+                                                                ON tt.`StudentID` = dt.`StudentID` AND tt.`DateAndTime` = dt.`DateNTime`
+                                                                    SET tt.`Count` = tt.`Count`+1");
+    $countUpdate -> execute();
+
+    echo "Time table count updated successfully";
+
+} catch (Exception $e) {
+    echo "Error: " . $e->getMessage();
 }
 
 
--- time_table.php
+++ time_table.php
@@ -2,7 +2,54 @@
     <title>Time Table</title>
 </head>
 <body>
-    <h1>Time Table</h1>
-    
+    <section class="container">
+        <div class="row">
+            <!-- Structre of  Time Table <-Start-> -->
+            <h1>Time Table</h1>
+            <?php
+                $currentDate = date('Y-m-d');
+                $currentDayName = date('l');
+                echo "<h2>$currentDayName 5.30 AM - 7.30 AM Students List ($currentDate)</h2>";
+            ?>
+            <table class="table table-bordered">
+                    <thead>
+                        <tr>
+                            <td>Student ID</td>
+                            <td>Student Name</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                            include "db_connection.php";
+
+                            $sql = $db->prepare("SELECT dt.`StudentID`,s.`StudentName` FROM `daily_time_tables` dt 
+                                                        JOIN `students` s ON dt.`StudentID` = s.`StudentID`
+                                                            WHERE dt.DateNTime = ?");
+                            $sql->execute(array('1'));
+                            while ($row = $sql -> fetch (PDO::FETCH_ASSOC)){      
+                        ?>
+                            <tr>
+                                <td><?php echo $row['ID'] ?></td>
+                                <td><?php echo $row['StudentID'] ?></td>
+                                <td><?php echo $row['StudentName'] ?></td>
+                                <td><?php echo $row['PhoneNumber'] ?></td>
+                                <td><?php echo $row['ProjectsCompleted'] ?></td>
+                                <td>
+                                    <a href=""><img class="buttonIcons" src="../images/view-eye-interface-symbol.png" alt="View Button"></a>
+                                    <a href=""><img class="buttonIcons" src="../images/edit-button.png" alt="Edit Button"></a>
+                                    <a href=""><img class="buttonIcons" src="../images/delete-button.png" alt="Delete Button"></a>
+                                </td>
+                            </tr>
+                        <?php
+                        }
+                        ?>
+                    </tbody>
+                </table>
+                <button type="submit" class="btn btn-success pull-right" onclick="Export()">
+                    <i class="fa fa-file-excel-o fa-fw">Export to Excel</i>
+                </button>
+                <!-- Structre of Time Table <-End-> -->
+        </div>
+    </section>
 </body>
 </html>
